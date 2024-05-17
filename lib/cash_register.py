55d3d2e142b91460a70a5e46fabb39867728669b
Tova Yocheved Hillman <hillman.t.y@gmail.com>
2024-05-17 10:42:29 -0400

    done

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  def __init__(self, discount=0, total=0):
+  def __init__(self, discount=0):
     self.discount = discount
-    self.total = total
+    self.total = 0
     self.prices= []
     self.items = []
     
